d2230cbc9f30aa91e52f9abf0d6984653a599db7
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2024-11-14 12:52:50 +0300

    Update views.py

--- measurement/views.py
+++ measurement/views.py
@@ -1,24 +1,24 @@
 # measurement/views.py
 from rest_framework import generics
 from .models import Sensor, Measurement
-from .serializers import SensorSerializer, SensorDetailSerializer, MeasurementSerializer
+from .serializers import Sensor_Serializer, Measurement_Serializer, Sensor_Detail_Serializer
 
 # Создать и получить список датчиков
 class SensorListCreateView(generics.ListCreateAPIView):
     queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
+    serializer_class = Sensor_Serializer
 
 
 # Получить, изменить и удалить конкретный датчик
 class SensorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Sensor.objects.all()
-    serializer_class = SensorDetailSerializer
+    serializer_class = Sensor_Detail_Serializer
 
 
 # Создать новое измерение
 class MeasurementCreateView(generics.CreateAPIView):
     queryset = Measurement.objects.all()
-    serializer_class = MeasurementSerializer
+    serializer_class = Measurement_Serializer
 
     def perform_create(self, serializer):
         sensor = Sensor.objects.get(id=self.request.data['sensor'])
