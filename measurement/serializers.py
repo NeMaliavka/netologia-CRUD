023673df161995ff65a55f7eb51ee23c67ab58f2
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2024-11-14 12:52:03 +0300

    Update serializers.py

--- measurement/serializers.py
+++ measurement/serializers.py
@@ -1,19 +1,19 @@
 from rest_framework import serializers
 from .models import Sensor, Measurement
 
-class MeasurementSerializer(serializers.ModelSerializer):
+class Measurement_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
         fields = ['temperature', 'created_at']
 
 
-class SensorSerializer(serializers.ModelSerializer):
+class Sensor_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = ['id', 'name', 'description']
 
 
-class SensorDetailSerializer(serializers.ModelSerializer):
+class Sensor_Detail_Serializer(serializers.ModelSerializer):
     measurements = MeasurementSerializer(many=True, read_only=True)
 
     class Meta:
